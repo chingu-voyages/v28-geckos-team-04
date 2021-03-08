3a7e97eb59b00f202398ffe205adecee0105fee7
iamtimleonard <timleonardcello@gmail.com>
2021-03-08 17:41:00 -0500

    search feature: move searchbar out of App.js into SearchBar.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,9 @@
-import { useState } from "react";
-import { getDataFromINat } from "./utils";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [iNatResults, setINatResults] = useState([]);
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { results } = await getDataFromINat(
-      "Morchella",
-      latitude,
-      longitude,
-      "30"
-    );
-    setINatResults(results);
-  };
-
-  const getLocation = (e) => {
-    e.preventDefault();
-    const success = async (pos) => {
-      const { latitude, longitude } = await pos.coords;
-      setLatitude(latitude);
-      setLongitude(longitude);
-    };
-    navigator.geolocation.getCurrentPosition(success);
-  };
-
   return (
     <div className="App">
-      <form onSubmit={handleSubmit} action="">
-        <input
-          value={latitude}
-          onChange={(e) => {
-            setLatitude(e.target.value);
-          }}
-          type="text"
-          placeholder="latitude"
-        />
-        <input
-          value={longitude}
-          onChange={(e) => {
-            setLongitude(e.target.value);
-          }}
-          type="text"
-          placeholder="longitue"
-        />
-        <button>Submit</button>
-        <button onClick={getLocation}>get my location</button>
-      </form>
-      <ul>Results</ul>
-      {iNatResults.map((result) => (
-        <li key={result.id}>{result.location}</li>
-      ))}
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { getDataFromINat } from "../utils";
+
+const SearchBar = () => {
+  const [iNatResults, setINatResults] = useState([]);
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { results } = await getDataFromINat(
+      "Morchella",
+      latitude,
+      longitude,
+      "30"
+    );
+    setINatResults(results);
+    console.log(results);
+  };
+  const getLocation = (e) => {
+    e.preventDefault();
+    const success = async (pos) => {
+      const { latitude, longitude } = await pos.coords;
+      setLatitude(latitude);
+      setLongitude(longitude);
+    };
+    navigator.geolocation.getCurrentPosition(success);
+  };
+  return (
+    <form onSubmit={handleSubmit} action="">
+      <input
+        value={latitude}
+        onChange={(e) => {
+          setLatitude(e.target.value);
+        }}
+        type="text"
+        placeholder="latitude"
+      />
+      <input
+        value={longitude}
+        onChange={(e) => {
+          setLongitude(e.target.value);
+        }}
+        type="text"
+        placeholder="longitue"
+      />
+      <button>Submit</button>
+      <button onClick={getLocation}>get my location</button>
+    </form>
+  );
+};
+
+export default SearchBar;
