6c647da6b6626eb9ccf813177e36dd780758cfbc
Myles Jeffery <mylesjeffery96@gmail.com>
2021-03-08 10:38:16 -0800

    google maps setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import { Map } from './components'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Map />
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/map/styles/mapStyles.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		featureType: 'all',
+		elementType: 'labels.text',
+		stylers: [
+			{
+				visibility: 'off',
+			},
+		],
+	},
+	{
+		featureType: 'all',
+		elementType: 'labels.icon',
+		stylers: [
+			{
+				visibility: 'off',
+			},
+		],
+	},
+]
