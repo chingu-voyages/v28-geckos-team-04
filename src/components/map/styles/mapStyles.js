430248d6bedafdaf5b0be905214912e358faa0d6
iamtimleonard <timleonardcello@gmail.com>
2021-03-08 20:37:38 -0500

    map feature: resolve error in mapStyles.js to deploy

--- src/components/map/styles/mapStyles.js
+++ src/components/map/styles/mapStyles.js
@@ -1,20 +1,22 @@
-export default [
-	{
-		featureType: 'all',
-		elementType: 'labels.text',
-		stylers: [
-			{
-				visibility: 'off',
-			},
-		],
-	},
-	{
-		featureType: 'all',
-		elementType: 'labels.icon',
-		stylers: [
-			{
-				visibility: 'off',
-			},
-		],
-	},
-]
+const mapStyles = [
+  {
+    featureType: "all",
+    elementType: "labels.text",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "all",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
+
+export default mapStyles;
