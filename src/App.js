76338c7c2b5af5424b2d4b1ac6af3e05c91a184e
iamtimleonard <timleonardcello@gmail.com>
2021-03-09 13:51:48 -0500

    map feature: auto pan to user location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route} from "react-router-dom"
+import { Route } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
 import NavBar from "./components/Navbar/NavBar";
 import Login from "./components/Login/Login";
@@ -8,8 +8,7 @@ import Register from "./components/Register/Register";
 import { Map } from "./components";
 import "./App.css";
 
-
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoggedIn: false,
   };
@@ -26,14 +25,17 @@ class App extends React.Component{
     });
   };
 
-
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
         <Route
           path="/"
           render={(props) => (
-            <NavBar {...props} {...this.state} handleLogout={this.handleLogout} />
+            <NavBar
+              {...props}
+              {...this.state}
+              handleLogout={this.handleLogout}
+            />
           )}
         />
         <Route
@@ -43,19 +45,17 @@ class App extends React.Component{
             <Login {...props} handleLogin={this.handleLogin} />
           )}
         />
-       <Route exact path="/register" component={Register} />
+        <Route exact path="/register" component={Register} />
         <Route exact path="/about" component={About} />
-      <main>
-        <section className="main">
-          <Route exact path="/home"  component={SearchBar}/>
-          <Map />
-        </section>
-      </main>
-    </div>
+        <main>
+          <section className="main">
+            <Route exact path="/home" component={SearchBar} />
+            <Map />
+          </section>
+        </main>
+      </div>
     );
   }
 }
-
-
 
 export default App;
