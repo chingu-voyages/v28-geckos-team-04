c7bc7f3423a40077ae52c85139d71dac4018b288
Tim Leonard <69484330+iamtimleonard@users.noreply.github.com>
2021-03-24 11:22:16 -0400

    Revert "infowindow observation photo, google maps link and date found"

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -76,96 +76,75 @@ function Map({ iNatResults, handleDrag, userLocation }) {
     });
   };
 
-	if (loadError) return 'Error loading map'
-	if (!isLoaded) return 'Loading map...'
-	return (
-		<div>
-			<CenterUserButton
-				userLocation={userLocation}
-				handleHomeButton={handleCenterUser}
-			/>
-			<PlacesSearch panTo={panTo} userLocation={userLocation} />
-			<GoogleMap
-				mapContainerStyle={mapContainerStyle}
-				zoom={11}
-				center={center}
-				options={options}
-				onLoad={onMapLoad}
-				onIdle={getNewBounds}
-				onClick={selected && (() => setSelected(null))}
-			>
-				{userLocation && (
-					<Marker
-						position={{
-							lat: userLocation.coords.latitude,
-							lng: userLocation.coords.longitude,
-						}}
-					/>
-				)}
-				{iNatResults.map((marker) => (
-					<Marker
-						key={marker.id}
-						position={{
-							lat: marker.geojson.coordinates[1],
-							lng: marker.geojson.coordinates[0],
-						}}
-						icon={{
-							url: '/mushroom.png',
-							scaledSize: new window.google.maps.Size(30, 30),
-						}}
-						onClick={() => {
-							setSelected(marker)
-						}}
-					/>
-				))}
+  if (loadError) return "Error loading map";
+  if (!isLoaded) return "Loading map...";
+  return (
+    <div>
+      <SpeciesSelect value={taxa} handleTaxaChange={handleTaxaChange} />
+      <CenterUserButton
+        userLocation={userLocation}
+        handleHomeButton={handleCenterUser}
+      />
+      <PlacesSearch panTo={panTo} />
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={11}
+        center={center}
+        options={options}
+        onLoad={onMapLoad}
+        onIdle={getNewBounds}
+        onClick={selected && (() => setSelected(null))}
+      >
+        {userLocation && (
+          <Marker
+            position={{
+              lat: userLocation.coords.latitude,
+              lng: userLocation.coords.longitude,
+            }}
+          />
+        )}
+        {iNatResults.map((marker) => (
+          <Marker
+            key={marker.id}
+            position={{
+              lat: marker.geojson.coordinates[1],
+              lng: marker.geojson.coordinates[0],
+            }}
+            icon={{
+              url: "/mushroom.png",
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
 
-				{selected && (
-					<InfoWindow
-						options={{
-							pixelOffset: new window.google.maps.Size(0, -30),
-						}}
-						position={{
-							lat: selected.geojson.coordinates[1],
-							lng: selected.geojson.coordinates[0],
-						}}
-						onCloseClick={() => {
-							setSelected(null)
-						}}
-					>
-						<div>
-							<h2>{selected.taxon.name}</h2>
-							<p>
-								Found at latitude:{selected.geojson.coordinates[1]}{' '}
-								longitude:
-								{selected.geojson.coordinates[0]}
-							</p>
-							<p>
-								Observed on: {selected.observed_on_details.month}/
-								{selected.observed_on_details.day}/
-								{selected.observed_on_details.year}
-							</p>
-							<p>Found by: {selected.user.login}</p>
-
-							<img
-								src={selected.photos[0].url.replace(
-									'square',
-									'medium'
-								)}
-								alt={`morel found by user ${selected.user.login}`}
-							/>
-							<br></br>
-							<a
-								href={`http://www.google.com/maps/place/${selected.geojson.coordinates[1]},${selected.geojson.coordinates[0]}`}
-								target={`_blank`}
-							>
-								View on Google Maps
-							</a>
-						</div>
-					</InfoWindow>
-				)}
-			</GoogleMap>
-		</div>
-	)
+        {selected && (
+          <InfoWindow
+            options={{
+              pixelOffset: new window.google.maps.Size(0, -30),
+            }}
+            position={{
+              lat: selected.geojson.coordinates[1],
+              lng: selected.geojson.coordinates[0],
+            }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>{selected.taxon.name}</h2>
+              <p>
+                found at latitude:{selected.geojson.coordinates[1]} longitude:
+                {selected.geojson.coordinates[0]}
+              </p>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </div>
+  );
 }
 
 export default Map;
