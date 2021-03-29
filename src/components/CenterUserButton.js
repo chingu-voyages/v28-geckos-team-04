d77ec640fded6b52cdac4c45b343164817a995f7
Myles Jeffery <mylesjeffery96@gmail.com>
2021-03-29 12:21:48 -0700

    icons and styling

--- src/components/CenterUserButton.js
+++ src/components/CenterUserButton.js
@@ -1,15 +1,12 @@
-import React from "react";
-import compass from "../assets/compass.png";
+import React from 'react'
+import compass from '../assets/compass.svg'
 
 const CenterUserButton = ({ userLocation, handleHomeButton }) => {
-  return (
-    <button
-      onClick={() => handleHomeButton(userLocation)}
-      className="home-button"
-    >
-      <img src={compass} alt="RETURN HOME" />
-    </button>
-  );
-};
+	return (
+		<button onClick={() => handleHomeButton(userLocation)} className="home-button">
+			<img src={compass} alt="RETURN HOME" />
+		</button>
+	)
+}
 
-export default CenterUserButton;
+export default CenterUserButton
--- src/components/Navbar/NavToggle.js
+++ src/components/Navbar/NavToggle.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React from 'react'
+import navButton from '../../assets/nav_button.svg'
 
 const NavToggle = ({ handleNavToggle }) => {
-  return <button onClick={handleNavToggle} className="nav-toggle"></button>;
-};
+	return (
+		<button onClick={handleNavToggle} className="nav-toggle">
+			<img src={navButton} alt="nav Button" />
+		</button>
+	)
+}
 
-export default NavToggle;
+export default NavToggle
