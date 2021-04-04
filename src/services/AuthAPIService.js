ef57b3da24e5663d9407193fff79565a8b98a8f5
iamtimleonard <timleonardcello@gmail.com>
2021-04-04 11:28:43 -0400

    adjust api url

--- src/services/AuthAPIService.js
+++ src/services/AuthAPIService.js
@@ -1,79 +1,71 @@
-import Config from '../config/Config'
-import TokenService from './TokenService'
-import IdleService from './IdleService'
+import TokenService from "./TokenService";
+import IdleService from "./IdleService";
 
 const AuthApiService = {
   postUser(user) {
-    return fetch(`${Config.REACT_APP_API_BASE_URL}/users`, {
-      method: 'POST',
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}/users`, {
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify(user),
-    })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
-      )
+    }).then((res) =>
+      !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+    );
   },
   postLogin({ username, password }) {
-    return fetch(`${Config.REACT_APP_API_BASE_URL}/auth/login`, {
-      method: 'POST',
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}/auth/login`, {
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
       body: JSON.stringify({ username, password }),
     })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
+      .then((res) =>
+        !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
       )
-      .then(res => {
+      .then((res) => {
         /*
           whenever a logint is performed:
           1. save the token in local storage
           2. queue auto logout when the user goes idle
           3. queue a call to the refresh endpoint based on the JWT's exp value
         */
-        TokenService.saveAuthToken(res.authToken)
-        IdleService.regiserIdleTimerResets()
+        TokenService.saveAuthToken(res.authToken);
+        IdleService.regiserIdleTimerResets();
         TokenService.queueCallbackBeforeExpiry(() => {
-          AuthApiService.postRefreshToken()
-        })
-        return res
-      })
+          AuthApiService.postRefreshToken();
+        });
+        return res;
+      });
   },
   postRefreshToken() {
-    return fetch(`${Config.REACT_APP_API_BASE_URL}/auth/refresh`, {
-      method: 'POST',
+    return fetch(`${process.env.REACT_APP_API_BASE_URL}/auth/refresh`, {
+      method: "POST",
       headers: {
-        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+        authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
     })
-      .then(res =>
-        (!res.ok)
-          ? res.json().then(e => Promise.reject(e))
-          : res.json()
+      .then((res) =>
+        !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
       )
-      .then(res => {
+      .then((res) => {
         /*
           similar logic to whenever a user logs in, the only differences are:
           - we don't need to queue the idle timers again as the user is already logged in.
           - we'll catch the error here as this refresh is happening behind the scenes
         */
-        TokenService.saveAuthToken(res.authToken)
+        TokenService.saveAuthToken(res.authToken);
         TokenService.queueCallbackBeforeExpiry(() => {
-          AuthApiService.postRefreshToken()
-        })
-        return res
+          AuthApiService.postRefreshToken();
+        });
+        return res;
       })
-      .catch(err => {
-        console.log('refresh token request error')
-        console.error(err)
-      })
+      .catch((err) => {
+        console.log("refresh token request error");
+        console.error(err);
+      });
   },
-}
+};
 
-export default AuthApiService
+export default AuthApiService;
